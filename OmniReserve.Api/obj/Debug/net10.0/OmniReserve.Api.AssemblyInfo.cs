8d9d107349c8faed758006635e35fe72e2e5e31e
jesus-martin22 <jmartin.26539@umsa.edu.mx>
2026-09-06 16:12:11 -0600

    Se instaló el paquete NuGet Swashbuckle en el archivo

--- OmniReserve.Api/obj/Debug/net10.0/OmniReserve.Api.AssemblyInfo.cs
+++ OmniReserve.Api/obj/Debug/net10.0/OmniReserve.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmniReserve.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d273c39cfec54cdb1c17144de17d7fac3d0e6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ab0383db9a0e4d199aa24bd8c6d1ccbd8a5d5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmniReserve.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmniReserve.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
